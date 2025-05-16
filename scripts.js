3f4dbea237b49aaa3172acd480cdf788c015f306
Ariel Barrozo <barrozo.ariel@inta.gob.ar>
2025-05-16 15:51:00 -0300

    formulario con API3

--- scripts.js
+++ scripts.js
@@ -1,20 +1,39 @@
 jQuery('document').ready(function($){
-    
-var menuBtn = $('.menu-icon'),
-    menu = $('.navigation ul');
 
-menuBtn.click(function(){
+  var menuBtn = $('.menu-icon'),
+      menu = $('.navigation ul');
 
-    if ( menu.hasClass('show') ){
-        
-        menu.removeClass('show');
+  menuBtn.click(function(){
+      if ( menu.hasClass('show') ){
+          menu.removeClass('show');
+      } else {
+          menu.addClass('show');
+      }
+  });
 
-    } else {
+  // Manejo del envío del formulario de contacto
+  $('#contactoForm').on('submit', function(e) {
+    e.preventDefault();
 
-        menu.addClass('show');
-        
-    }
-    
-});
+    const nombre = $('#nombre').val();
+    const email = $('#email').val();
+    const mensaje = $('#mensaje').val();
+
+    fetch('/api/enviarcorreo', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ nombre, email, mensaje })
+    })
+      .then(res => res.json())
+      .then(data => {
+        alert(data.message);
+      })
+      .catch(error => {
+        console.error('Error:', error);
+        alert('Hubo un problema al enviar el mensaje.');
+      });
+  });
 
 });
